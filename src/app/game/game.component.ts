be12e4c7c9713c12b5f063e229db3e5655eb4385
Andrea Mazzini <andrea.mazzini00@gmail.com>
2023-11-26 16:43:34 +0100

    Refactor game component HTML and styles

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,24 @@
 import { Component, Input } from '@angular/core';
 
+import { PopulationServiceService } from '../services/population-service.service';
+import { GameServiceService } from '../services/game-service.service';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
-  @Input({required: true})
-  started!: boolean;
+  @Input({ required: true })
+  started: boolean;
+
+  constructor(public populationService: PopulationServiceService,
+    public gameService: GameServiceService
+  ) {
+    this.started = this.gameService.isStarted();
+  }
+
+  ngOnInit(): void {
+    this.populationService.getInitialElements();
+  }
 }
